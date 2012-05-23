466ec8b0099a2b9d62fc63dc655520211ede420c
Florian Schulze <florian.schulze@gmx.net>
2012-05-23 19:22:56 +0200

    Prepare mr.laforge 0.5.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-version = '0.4'
+version = '0.5'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
